7930f87ae0a0dc7a12756fd1b289d085338ca0e2
Muhammad Asim Javed <47146912+asimj14@users.noreply.github.com>
2021-06-29 21:57:52 +0100

    Tenth Working Copy
    - Statistics Page Implemented
    - TableView Implemented
    - Toolbar Improved

--- DiabetesManagementApplication/app/src/main/java/uk/ac/westminster/diabetesmanagementapplication/StatisticsActivity.java
+++ DiabetesManagementApplication/app/src/main/java/uk/ac/westminster/diabetesmanagementapplication/StatisticsActivity.java
@@ -10,6 +10,9 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -27,7 +30,15 @@ public class StatisticsActivity extends AppCompatActivity implements NavigationV
     NavigationView navigationview;
     Toolbar toolbar;
     String userName, userEmail;
-
+    TextView labelLow,labelNormal,labelHigh, labelExhigh;
+    TextView textViewLow,textViewNormal,textViewHigh,textViewExHigh;
+    int lowCount=0,normalCount=0,highCount=0,extraHighCount=0;
+    DBHelper db;
+    SQLiteDatabase sqLiteDatabase;
+    String[] glucose;
+    String[] recordDate;
+    String[] recordTime;
+    int[] id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +50,65 @@ public class StatisticsActivity extends AppCompatActivity implements NavigationV
         drawerLayout = findViewById(R.id.drawerlayout);
         navigationview = findViewById(R.id.navigationview);
         toolbar = findViewById(R.id.toolbar);
+        textViewLow = findViewById(R.id.txt_low_levels);
+        textViewNormal = findViewById(R.id.txt_normal_levels);
+        textViewHigh = findViewById(R.id.txt_high_levels);
+        textViewExHigh = findViewById(R.id.txt_extra_high_levels);
+        labelLow = findViewById(R.id.labellow);
+        labelHigh = findViewById(R.id.labelhigh);
+        labelNormal = findViewById(R.id.labelnormal);
+        labelExhigh = findViewById(R.id.labelexhigh);
+
+        //Statistics
+
+        SharedPreferences preferencesUid = getSharedPreferences("useriddetails", MODE_PRIVATE);
+        Integer userid = preferencesUid.getInt("userid", 0);
+        String userID = Integer.toString(userid);
+        db = new DBHelper(this);
+
+        sqLiteDatabase = db.getWritableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM glucose WHERE patientId=?", new String[]{userID});
+
+        if (cursor.getCount() > 0) {
+            id = new int[cursor.getCount()];
+            glucose = new String[cursor.getCount()];
+            recordDate = new String[cursor.getCount()];
+            recordTime = new String[cursor.getCount()];
+            int i = cursor.getCount() - 1;
+            while (cursor.moveToNext()) {
+                id[i] = cursor.getInt(0);
+                glucose[i] = cursor.getString(1);
+                if(Integer.parseInt(glucose[i]) <= 80){
+                    lowCount++;
+
+                }else if((Integer.parseInt(glucose[i]) > 80) && (Integer.parseInt(glucose[i]) <= 115)){
+                    normalCount++;
+
+                }else if((Integer.parseInt(glucose[i]) > 115) && (Integer.parseInt(glucose[i]) < 180)){
+                    highCount++;
+
+                }else if((Integer.parseInt(glucose[i]) >= 180)) {
+                    extraHighCount++;
+                }
+
+                recordDate[i] = cursor.getString(2);
+                recordTime[i] = cursor.getString(3);
+                i--;
+            }
+        }
+
+        textViewLow.setText(Integer.toString(lowCount));
+        labelLow.setBackgroundColor(Color.rgb(245, 199, 0));
+        textViewLow.setBackgroundColor(Color.rgb(245, 199, 0));
+        textViewNormal.setText(Integer.toString(normalCount));
+        labelNormal.setBackgroundColor(Color.rgb(106, 150, 31));
+        textViewNormal.setBackgroundColor(Color.rgb(106, 150, 31));
+        textViewHigh.setText(Integer.toString(highCount));
+        labelHigh.setBackgroundColor(Color.rgb(255, 102, 0));
+        textViewHigh.setBackgroundColor(Color.rgb(255, 102, 0));
+        textViewExHigh.setText(Integer.toString(extraHighCount));
+        labelExhigh.setBackgroundColor(Color.rgb(193, 37, 82));
+        textViewExHigh.setBackgroundColor(Color.rgb(193, 37, 82));
 
         //Side Navigator
         navigationview.bringToFront();
